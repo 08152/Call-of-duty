5bb5db71a84b7eb95a28295e2baf97dfe8a7ed52
08152 <RiskyLive@proton.me>
2026-06-14 13:56:49 +0200

    Update ecs.js

--- engine/ecs.js
+++ engine/ecs.js
@@ -1,21 +1,217 @@
 export class ECS{
+
 constructor(){
+
 this.entities=[];
+
+this.systems=[];
+
+this.nextId=1;
+
 }
 
-create(){
-const e={id:crypto.randomUUID(),components:{}};
-this.entities.push(e);
-return e;
+/* =========================
+   ENTITY
+========================= */
+
+createEntity(){
+
+const entity={
+
+id:this.nextId++,
+
+components:{}
+
+};
+
+this.entities.push(entity);
+
+return entity;
+
 }
 
-add(e,name,data){
-e.components[name]=data;
+removeEntity(entity){
+
+this.entities=
+this.entities.filter(
+e=>e.id!==entity.id
+);
+
 }
 
-get(e,name){
-return e.components[name];
-}
+/* =========================
+   COMPONENTS
+========================= */
+
+addComponent(
+entity,
+name,
+data
+){
+
+entity.components[name]=data;
+
 }
 
-export const ecs=new ECS();
+removeComponent(
+entity,
+name
+){
+
+delete entity.components[name];
+
+}
+
+getComponent(
+entity,
+name
+){
+
+return entity.components[name];
+
+}
+
+hasComponent(
+entity,
+name
+){
+
+return entity.components[name]
+!==undefined;
+
+}
+
+/* =========================
+   SYSTEMS
+========================= */
+
+addSystem(system){
+
+this.systems.push(system);
+
+}
+
+update(dt){
+
+for(const system of this.systems){
+
+system.update(
+dt,
+this.entities
+);
+
+}
+
+}
+
+/* =========================
+   QUERIES
+========================= */
+
+query(...components){
+
+return this.entities.filter(entity=>{
+
+for(const component of components){
+
+if(
+!entity.components[
+component
+]
+){
+
+return false;
+
+}
+
+}
+
+return true;
+
+});
+
+}
+
+}
+
+/* =========================
+   COMPONENT HELPERS
+========================= */
+
+export const Components={
+
+Transform(
+position,
+rotation,
+scale
+){
+
+return{
+
+position,
+
+rotation,
+
+scale
+
+};
+
+},
+
+Velocity(
+velocity
+){
+
+return{
+
+velocity
+
+};
+
+},
+
+Health(
+health=100
+){
+
+return{
+
+health
+
+};
+
+},
+
+Projectile(
+damage=25
+){
+
+return{
+
+damage
+
+};
+
+},
+
+Enemy(){
+
+return{
+
+alive:true
+
+};
+
+},
+
+Barrier(){
+
+return{
+
+solid:true
+
+};
+
+}
+
+};
